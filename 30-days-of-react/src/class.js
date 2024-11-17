6587d20f7224d360d93cbfaa0912f5af54df33aa
Billy <billyLin1954@gmail.com>
2024-11-16 20:02:34 -0500

    f

---
+++ 30-days-of-react/src/class.js
@@ -0,0 +1,42 @@
+class Parent {
+    constructor(firstName, lastName, country, title) {
+      // we bind the params with this class object using this keyword
+      this.firstName = firstName
+      this.lastName = lastName
+      this.country = country
+      this.title = title
+    }
+    getPersonInfo() {
+      return `${this.firstName} ${this.lastName},  a ${this.title} developer base in ${this.country} `
+    }
+    parentMethod() {
+      // code goes here
+    }
+  }
+  
+  const p1 = new Parent('Asabeneh', 'Yetayeh', 'Finland', 'FullStack Developer')
+  
+  class Child extends Parent {
+    constructor(firstName, lastName, country, title, skills) {
+      super(firstName, lastName, country, title)
+      this.skills = skills
+      // we bind the child params with the this keyword to this child object
+    }
+    getSkills() {
+      let len = this.skills.length
+      return len > 0 ? this.skills.join(' ') : 'No skills found'
+    }
+    childMethod() {
+      // code goes here
+    }
+  }
+  
+  const skills = ['HTML', 'CSS', 'JS', 'React']
+  
+  const child = new Child(
+    'Asabeneh',
+    'Yetayeh',
+    'Finland',
+    'FullStack Developer',
+    skills
+  )
--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -1,35 +1,31 @@
+// index.js
 import React from 'react'
 import ReactDOM from 'react-dom'
-const headerStyles = {
-    backgroundColor: '#61DBFB',
-    fontFamily: 'Helvetica Neue',
-    padding: 25,
-    lineHeight: 1.5,
-  }
-  const mainstyle ={
-    backgroundColor: '#F3F0F5',
-  }
-const amazing=<main style={mainstyle}>
- <div>
-    <p>what is needed for React</p>
-    <ul>
-        <li>JavaScript</li>
-        <li>HTML</li>
-        <li>CSS</li>
-    </ul>
- </div>
-</main>
-const jsxElement = <header style={headerStyles}>
-<h1>Welcome to 30 Days Of React</h1>
-<h2>Getting Started React</h2>
-<h3>JavaScript Library</h3>
-<p>Asabeneh Yetayeh</p>
-<small>Oct 2, 2020</small>
-</header>
-const output=(<div class="app">
-    {jsxElement}
-    {amazing}
-</div>)
-const rootElement = document.getElementById('root')
 
-ReactDOM.render(output, rootElement)
+// Header Component
+const Header = () => (
+  <header>
+    <div className='header-wrapper'>
+      <h1>Welcome to 30 Days Of React</h1>
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Asabeneh Yetayeh</p>
+      <small>Oct 3, 2020</small>
+      < TechList/>
+      <List/>
+    </div>
+  </header>
+)
+const TechList = () => {
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+  const List = () => {
+      const stuff= (<h1>sup</h1>)
+     return stuff
+  }
+  
+const rootElement = document.getElementById('root')
+// we render the JSX element using the ReactDOM package
+ReactDOM.render(<Header />, rootElement)
